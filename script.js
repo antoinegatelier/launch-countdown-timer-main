8079c75c4328bc7519433d8798b41adc5e3fa219
Antoine Gatelier <gatelier.a@gmail.com>
2021-05-18 19:47:10 +0200

    add README and factorize functions

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 /* Constants */
-const launchDate = new Date(2021, 4, 20, 17, 30, 00);
 
+//set your own date: year, month - 1, day, hour, minute, second
+//const launchDate = new Date(2021, 4, 20, 17, 30, 00);
+
+//set launchDate to 14 days at page loading - 1000 (ms) * 3600 (s = 1h) * 24 (hours) * 14 = 14days
+const launchDate = Date.now() + 1000 * 3600 * 24 * 14;
+
+//converts milliseconds to seconds/minutes/hours/days
 const msToSeconds = 1000;
 const msToMinutes = 60 * msToSeconds;
 const msToHours = 60 * msToMinutes;
 const msToDays = 24 * msToHours;
+
+/* Variables */
+
+const days = { 
+    value: '',
+    update: false,
+    zIndex: [document.querySelector('#z0d'), document.querySelector('.z1d'), document.querySelector('#z2d'), document.querySelector('.z3d')]
+};
+
+const hours = {
+    value: '',
+    update: false,
+    zIndex: [document.querySelector('#z0h'), document.querySelector('.z1h'), document.querySelector('#z2h'), document.querySelector('.z3h')]
+};
+
+const minutes = {
+    value: '',
+    update: false,
+    zIndex: [document.querySelector('#z0m'), document.querySelector('.z1m'), document.querySelector('#z2m'), document.querySelector('.z3m')]
+};
+
+const seconds = {
+    value: '',
+    update: false,
+    zIndex: [document.querySelector('#z0s'), document.querySelector('.z1s'), document.querySelector('#z2s'), document.querySelector('.z3s')]
+};
 
 /* Helper functions */
 
@@ -31,85 +63,41 @@ const toTwoDigits = (number) => {
         return `0${number}`;
     }
     return number;
-}
+};
 
-const updateText = (nodeToUpdate, unitToUpdate) => {
-    nodeToUpdate.forEach(node => node.textContent = toTwoDigits(unitToUpdate));
-}
+const updateText = (timeUnit, index) => {
+    timeUnit.zIndex[index].textContent = toTwoDigits(timeUnit.value);
+};
+
+const updateTimeUnit = (timeUnit) => {
+    if(timeUnit.update) {
+        updateText(timeUnit, 1);
+        updateText(timeUnit, 2);
+        timeUnit.zIndex[3].animate(animateTop, duration = 950, iterations = 1);
+        timeUnit.zIndex[1].animate(animateBottom, duration = 950, iterations = 1);
+        window.setTimeout(() => {
+            updateText(timeUnit, 3);
+            updateText(timeUnit, 0);
+        }, 750);
+        timeUnit.update = false;
+    }
+};
 
 /* Animations */
 
 const animateTop = [
-    { transformOrigin: 'bottom', transform: 'scale(1 ,1)' },
+    { transformOrigin: 'bottom', transform: 'scale(1, 1)' },
     { transformOrigin: 'bottom', transform: 'scale(1, 0)', offset: 0.5},
     { transformOrigin: 'bottom', transform: 'scale(1, 0)'}
 ];
 const animateBottom = { transform: ['scale(1, 0)', 'scale(1, 0)', 'scale(1, 1)'] };
 
-/* Selectors */
-
-const daysTopDivsSelector = document.querySelectorAll(".daysFirst");
-const daysBottomDivsSelector = document.querySelectorAll(".daysSecond");
-const hoursTopDivsSelector = document.querySelectorAll(".hoursFirst");
-const hoursBottomDivsSelector = document.querySelectorAll(".hoursSecond");
-const minutesTopDivsSelector = document.querySelectorAll(".minutesFirst");
-const minutesBottomDivsSelector = document.querySelectorAll(".minutesSecond");
-const secondsTopDivsSelector = document.querySelectorAll(".secondsFirst");
-const secondsBottomDivsSelector = document.querySelectorAll(".secondsSecond");
-
-/* Variables */
-
-const days = { value: '', update: false };
-const hours = { value: '', update: false };
-const minutes = { value: '', update: false};
-const seconds = { value: '', update: false};
-
 /* Defines and starts counter - refreshes every second (1000ms) */
 
 const counter = setInterval(() => {
     updateRemainingTime();
-    if(days.update) {
-        document.querySelector('.z1d').textContent = toTwoDigits(days.value);
-        document.querySelector('#z2d').textContent = toTwoDigits(days.value);
-        document.querySelector('.z3d').animate(animateTop, duration = 950, iterations = 1);
-        document.querySelector('.z1d').animate(animateBottom, duration = 950, iterations = 1);
-        window.setTimeout(() => {
-            document.querySelector('.z3d').textContent = toTwoDigits(days.value);
-            document.querySelector('#z0d').textContent = toTwoDigits(days.value);
-        }, 750);
-        days.update = false;
-        };
-    if(hours.update) {
-        document.querySelector('.z1h').textContent = toTwoDigits(hours.value);
-        document.querySelector('#z2h').textContent = toTwoDigits(hours.value);
-        document.querySelector('.z3h').animate(animateTop, duration = 950, iterations = 1);
-        document.querySelector('.z1h').animate(animateBottom, duration = 950, iterations = 1);
-        window.setTimeout(() => {
-            document.querySelector('.z3h').textContent = toTwoDigits(hours.value);
-            document.querySelector('#z0h').textContent = toTwoDigits(hours.value);
-        }, 750);
-        hours.update = false;
-    };
-    if(minutes.update) {
-        document.querySelector('.z1m').textContent = toTwoDigits(minutes.value);
-        document.querySelector('#z2m').textContent = toTwoDigits(minutes.value);
-        document.querySelector('.z3m').animate(animateTop, duration = 950, iterations = 1);
-        document.querySelector('.z1m').animate(animateBottom, duration = 950, iterations = 1);
-        window.setTimeout(() => {
-            document.querySelector('.z3m').textContent = toTwoDigits(minutes.value);
-            document.querySelector('#z0m').textContent = toTwoDigits(minutes.value);
-        }, 750);
-        minutes.update = false;
-    };
-    if(seconds.update) {
-        document.querySelector('.z1s').textContent = toTwoDigits(seconds.value);
-        document.querySelector('#z2s').textContent = toTwoDigits(seconds.value);
-        document.querySelector('.z3s').animate(animateTop, duration = 950, iterations = 1);
-        document.querySelector('.z1s').animate(animateBottom, duration = 950, iterations = 1);
-        window.setTimeout(() => {
-            document.querySelector('.z3s').textContent = toTwoDigits(seconds.value);
-            document.querySelector('#z0s').textContent = toTwoDigits(seconds.value);
-        }, 750);
-        seconds.update = false;
-    }
+    updateTimeUnit(days);
+    updateTimeUnit(hours);
+    updateTimeUnit(minutes);
+    updateTimeUnit(seconds);
 }, 1000);
